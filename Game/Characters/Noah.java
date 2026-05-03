5edb0c627ed928166d37889fa24181a6ebccb3c1
Jasmine Sarsaba <jaja.srsb@gmail.com>
2026-05-04 00:19:37 +0800

    feat(characters): refactor Noah class and add empty biography override

--- Game/Characters/Noah.java
+++ Game/Characters/Noah.java
@@ -1,65 +1,71 @@
 package game.characters;
 
 public class Noah extends Character {
-    public Noah(){
-        super("Noah", 400, 20,100);
-        super.skill1 = "Terrorist Barrage";
-        super.skill2 = "Air Strike";
-        super.skill3 = "Dual Wield";
 
-        //level
-        super.level = 1;
+    public Noah() {
+        super("Noah", 400, 20, 100);
 
-        //kani min-max dmg per skill
-        super.skill1DmgMin = 25;
-        super.skill1DmgMax = 45;
+        skill1 = "Terrorist Barrage";
+        skill2 = "Air Strike";
+        skill3 = "Dual Wield";
 
-        super.skill2DmgMin = 80;
-        super.skill2DmgMax = 100;
+        level = 1;
 
-        super.skill3DmgMin = 400;
-        super.skill3DmgMax = 800;
+        skill1DmgMin = 25;
+        skill1DmgMax = 45;
 
-        super.maxSkill1Cost = 0.15f;
-        super.maxSkill2Cost = 0.35f;
-        super.maxSkill3Cost = 1;
+        skill2DmgMin = 80;
+        skill2DmgMax = 100;
 
-        super.skill2CD = 2;
-        super.skill3CD = 4;
-    }
-  
-    @Override
-    public void Encounter(){
-        String CharacterNoah = "You picked Noah.. \n\nElara: Now, Noah... go. The first Shard is in the deepest canyon, at the citadel of\n" +
-                "Yoshul. And I wish you the Best.";
+        skill3DmgMin = 400;
+        skill3DmgMax = 800;
 
-        System.out.println(CharacterNoah);
+        maxSkill1Cost = 0.15f;
+        maxSkill2Cost = 0.35f;
+        maxSkill3Cost = 1f;
 
+        skill2CD = 2;
+        skill3CD = 4;
     }
 
+    // ilisanan pani ug unsa jud ila i sulti kay wapata kakhibaw unsa jud ila specific story ngano ga away ni sila ahahhhha
     @Override
-    public float Skill1(int manaUsage){
+    public void Encounter() {
+        System.out.println(
+                "You picked Noah..\n\n" +
+                        "Elara: Now, Noah... go. The first Shard is in the deepest canyon, at the citadel of Yoshul."
+        );
+    }
+
+    // short story nis character gais, gi abstract ni siyaa so need mag specific implement each  character
+    @Override
+    public void Biography() {
+
+    }
+
+    @Override
+    public float Skill1(int manaUsage) {
         setMP(getMP() - manaUsage);
-        float damage = randomDmg(skill1DmgMin , skill1DmgMax);
-        System.out.println("Player used (" + skill1 + ") Damage: [" +  (int)damage + ']');
-        return damage;
-    }
-    @Override
-    public float Skill2(int manaUsage){
-        super.skill2CD = 2;
-        setMP(getMP() - manaUsage);
-        float damage = randomDmg(skill2DmgMin , skill2DmgMax);
-        System.out.println("Player used (" + skill2 + ") Damage: [" +  (int)damage + ']');
-        return damage;
-    }
-    @Override
-    public float Skill3(int manaUsage){
-        super.skill3CD = 5;
-        setMP(getMP() - manaUsage);
-        float damage = randomDmg(skill3DmgMin , skill3DmgMax);
-        System.out.println("Player used (" + skill3 + ") Damage: [" +  (int)damage + ']');
+        float damage = randomDmg(skill1DmgMin, skill1DmgMax);
+        System.out.println("Player used (" + skill1 + ") Damage: [" + (int) damage + "]");
         return damage;
     }
 
+    @Override
+    public float Skill2(int manaUsage) {
+        skill2CD = 2;
+        setMP(getMP() - manaUsage);
+        float damage = randomDmg(skill2DmgMin, skill2DmgMax);
+        System.out.println("Player used (" + skill2 + ") Damage: [" + (int) damage + "]");
+        return damage;
+    }
 
+    @Override
+    public float Skill3(int manaUsage) {
+        skill3CD = 5;
+        setMP(getMP() - manaUsage);
+        float damage = randomDmg(skill3DmgMin, skill3DmgMax);
+        System.out.println("Player used (" + skill3 + ") Damage: [" + (int) damage + "]");
+        return damage;
+    }
 }
