f50f2e6736d0e91ace6ec865b333e3b29a2039f3
Sebial <maryrose.sebial@cit.edu>
2026-05-03 23:58:35 +0800

    feat(character): add Ashe character
    - skills and damage
    - mana cost and cooldown
    - encounter dialogue

--- Game/Characters/Ashe.java
+++ Game/Characters/Ashe.java
@@ -1,65 +1,70 @@
 package game.characters;
 
-import game.characters.Character;
-
 public class Ashe extends Character {
+
     public Ashe() {
-        super("Ashe",350, 150,150);
-        super.skill1 = "Frost Shot";
-        super.skill2 = "";
-        super.skill3 = "Enchanted Crystal Arrow";
+        super("Ashe", 350, 150, 150);
 
-        // level
-        super.level = 1;
+        skill1 = "Frost Shot";
+        skill2 = "Ice Barrier";
+        skill3 = "Enchanted Crystal Arrow";
 
-        // sa min-max dmg per skill
-        super.skill1DmgMin = 25;
-        super.skill1DmgMax = 45;
+        level = 1;
 
-        super.skill2DmgMin = 80;
-        super.skill2DmgMax = 100;
+        skill1DmgMin = 25;
+        skill1DmgMax = 45;
 
-        super.skill3DmgMin = 275;
-        super.skill3DmgMax = 325;
+        skill2DmgMin = 80;
+        skill2DmgMax = 100;
 
-        super.maxSkill1Cost = 0.2f;
-        super.maxSkill2Cost = 0.35f;
-        super.maxSkill3Cost = 0.5f;
+        skill3DmgMin = 275;
+        skill3DmgMax = 325;
 
-        super.skill2CD = 1;
-        super.skill3CD = 2;
+        maxSkill1Cost = 0.2f;
+        maxSkill2Cost = 0.35f;
+        maxSkill3Cost = 0.5f;
+
+        skill2CD = 1;
+        skill3CD = 2;
     }
 
     @Override
-    public void Encounter(){
-        String CharacterAshe = "You picked Ashe.. \n\nElara: Now, Ashe... go. The first Shard is in the deepest canyon, at the citadel of\n" +
-                "Yoshul. And I wish you the Best.";
+    public void Encounter() {
+        System.out.println(
+                "You picked Ashe..\n\n" +
+                        "Elara: Now, Ashe... go. The first Shard is in the deepest canyon, at the citadel of Yoshul."
+        );
+    }
 
-        System.out.println(CharacterAshe);
+    // biography 
+    @Override
+    public void Biography() {
 
     }
 
     @Override
-    public float Skill1(int manaUsage){
+    public float Skill1(int manaUsage) {
         setMP(getMP() - manaUsage);
-        float damage = randomDmg(skill1DmgMin , skill1DmgMax);
-        System.out.println("Player used (" + skill1 + ") Damage: [" +  (int)damage + ']');
+        float damage = randomDmg(skill1DmgMin, skill1DmgMax);
+        System.out.println("Player used (" + skill1 + ") Damage: [" + (int) damage + "]");
         return damage;
     }
+
     @Override
-    public float Skill2(int manaUsage){
-        super.skill2CD = 1;
+    public float Skill2(int manaUsage) {
+        skill2CD = 1;
         setMP(getMP() - manaUsage);
-        float damage = randomDmg(skill2DmgMin , skill2DmgMax);
-        System.out.println("Player used (" + skill2 + ") Damage: [" +  (int)damage + ']');
+        float damage = randomDmg(skill2DmgMin, skill2DmgMax);
+        System.out.println("Player used (" + skill2 + ") Damage: [" + (int) damage + "]");
         return damage;
     }
+
     @Override
-    public float Skill3(int manaUsage){
-        super.skill3CD = 2;
+    public float Skill3(int manaUsage) {
+        skill3CD = 2;
         setMP(getMP() - manaUsage);
-        float damage = randomDmg(skill3DmgMin , skill3DmgMax);
-        System.out.println("Player used (" + skill3 + ") Damage: [" +  (int)damage + ']');
+        float damage = randomDmg(skill3DmgMin, skill3DmgMax);
+        System.out.println("Player used (" + skill3 + ") Damage: [" + (int) damage + "]");
         return damage;
     }
 }
